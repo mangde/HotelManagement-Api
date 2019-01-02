26f550187f7d0c900d8c9ed4737ffd0c9c6fdddc
yogesh mangde <36396427+mangde@users.noreply.github.com>
2019-01-02 13:00:59 +0530

    Handle CustomerService Exception

--- hotelmanagement/src/main/java/com/nuance/him/Exception/CustomerServicesException.java
+++ hotelmanagement/src/main/java/com/nuance/him/Exception/CustomerServicesException.java
@@ -1,7 +1,3 @@
-/*
- * COPYRIGHT: Copyright (c) 2018 by Nuance Communications, Inc.* COPYRIGHT: Copyright (c) 2018 by Nuance Communications, Inc.
- * Warning: This product is protected by United States copyright law. Unauthorized use or duplication of this software, in whole or in part, is prohibited.
- */
 
 package com.nuance.him.Exception;
 
