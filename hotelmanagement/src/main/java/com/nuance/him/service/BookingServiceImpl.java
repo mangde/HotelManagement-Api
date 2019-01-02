6eac64a16bf1ab116db3734dd09148f947dc5178
yogesh mangde <36396427+mangde@users.noreply.github.com>
2019-01-02 12:52:30 +0530

    implement BookingService Interface methods

--- hotelmanagement/src/main/java/com/nuance/him/service/BookingServiceImpl.java
+++ hotelmanagement/src/main/java/com/nuance/him/service/BookingServiceImpl.java
@@ -1,8 +1,4 @@
-/*
- * COPYRIGHT: Copyright (c) 2018 by Nuance Communications, Inc.
- *  Warning: This product is protected by United States copyright law. Unauthorized use or duplication of this software, in whole or in part, is prohibited.
- *
- */
+
 
 package com.nuance.him.service;
 
